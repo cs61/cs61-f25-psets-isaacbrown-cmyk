36c11725874995ec014a5c7987fba814c920959a
Isaac Brown <isaacbrown@college.harvard.edu>
2025-11-03 17:25:59 -0300

    while loop

--- pset0/bc61.cc
+++ pset0/bc61.cc
@@ -2,8 +2,9 @@
 #include <cstdio>
 int main() {
     int i = 0;
-    fgetc(stdin);
-    i = i + 1;
+    while (fgetc(stdin) != -1) {
+        i = i + 1;
+    }
     fprintf(stdout, "Number of bytes: %d\n", i);
     exit(0);
 }
