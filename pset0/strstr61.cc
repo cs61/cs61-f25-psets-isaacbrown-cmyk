05c7730389c34fa1f1116a4ca901dca685c2a229
Isaac Brown <isaacbrown@college.harvard.edu>
2025-11-03 18:23:39 -0300

    testing

--- pset0/strstr61.cc
+++ pset0/strstr61.cc
@@ -3,15 +3,17 @@
 #include <cstdio>
 
 char* mystrstr(const char* s1, const char* s2) {
+    fprintf(stdout, s2[0])
     // Your code here
+
     return nullptr;
 }
 
 int main(int argc, char* argv[]) {
     assert(argc == 3);
-    printf("strstr(\"%s\", \"%s\") = %p\n",
-           argv[1], argv[2], strstr(argv[1], argv[2]));
+    //printf("strstr(\"%s\", \"%s\") = %p\n",
+           //argv[1], argv[2], strstr(argv[1], argv[2]));
     printf("mystrstr(\"%s\", \"%s\") = %p\n",
            argv[1], argv[2], mystrstr(argv[1], argv[2]));
-    assert(strstr(argv[1], argv[2]) == mystrstr(argv[1], argv[2]));
+    //assert(strstr(argv[1], argv[2]) == mystrstr(argv[1], argv[2]));
 }
