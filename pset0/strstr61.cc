e85b5a0192d7d35fe09df83f8eb6c7a26156b1e3
Isaac Brown <isaacbrown@college.harvard.edu>
2025-11-03 18:32:06 -0300

    bwah 2

--- pset0/strstr61.cc
+++ pset0/strstr61.cc
@@ -22,7 +22,7 @@ char* mystrstr(const char* s1, const char* s2) {
 int main(int argc, char* argv[]) {
     assert(argc == 3);
     printf("strstr(\"%s\", \"%s\") = %p\n",
-           //argv[1], argv[2], strstr(argv[1], argv[2]));
+           argv[1], argv[2], strstr(argv[1], argv[2]));
     printf("mystrstr(\"%s\", \"%s\") = %p\n",
            argv[1], argv[2], mystrstr(argv[1], argv[2]));
     assert(strstr(argv[1], argv[2]) == mystrstr(argv[1], argv[2]));
