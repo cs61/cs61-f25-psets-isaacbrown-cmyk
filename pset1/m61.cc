a737308e877d64b78d7f4dce19c839bda7b33544
Isaac Brown <isaacbrown@college.harvard.edu>
2025-11-03 23:22:13 -0300

    Maybe?

--- pset1/m61.cc
+++ pset1/m61.cc
@@ -75,7 +75,7 @@ static void* m61_find_free_space(size_t sz) {
         return nullptr;
     }
     for(auto const & [k,v]: freed_sizes){
-        std::cout << "Key: " << k << " Value: " << v << std::endl;
+        std::cout << "Pointer: " << k << " Size of allocation: \n" << v << std::endl;
         if(v >= sz){
             freed_sizes.erase(k);
             return k;
