8d88f80345133827a1c2a605fd7503277915057b
Isaac Brown <isaacbrown@college.harvard.edu>
2025-11-03 22:10:37 -0300

    nullptr

--- pset1/test10.cc
+++ pset1/test10.cc
@@ -8,7 +8,9 @@ int main() {
     char* p = (char*) m61_malloc(10);
     m61_statistics stat = m61_get_statistics();
     fprintf(stdout, "Heap max: %ld, Heap min: %ld\n", stat.heap_max, stat.heap_min);
-    fprintf(stdout, "Returned ptr: %s\n",p);
+    if(p == nullptr){
+        fprintf(stdout, "Returned ptr: %s\n",p);
+    }
     assert((uintptr_t) p >= stat.heap_min);
     assert((uintptr_t) p + 9 <= stat.heap_max);
     m61_free(p);
