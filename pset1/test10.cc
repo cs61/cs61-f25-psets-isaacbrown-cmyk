78f473bf29fde1a8a1061191218f6ac1ac06983d
Isaac Brown <isaacbrown@college.harvard.edu>
2025-11-03 21:57:55 -0300

    debugging

--- pset1/test10.cc
+++ pset1/test10.cc
@@ -8,7 +8,7 @@ int main() {
     char* p = (char*) m61_malloc(10);
 
     m61_statistics stat = m61_get_statistics();
-    m61_print_statistics();
+    fprintf(stdout, "Heap max: %d, Heap min: %d\n", stat.heap_max, stat.heap_min);
     //assert((uintptr_t) p >= stat.heap_min);
     //assert((uintptr_t) p + 9 <= stat.heap_max);
 
